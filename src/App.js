c0eff345313a02b7f699290d90eaa31296d7f9a9
oscar anillo <oscarani14@gmail.com>
2022-08-16 20:10:07 -0500

    add animation to the app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import TextComponent from './components/text-component';
 import CardComponent from './components/card-component';
 import { pageData } from './data/page-data';
 
+import 'animate.css';
+
 import './App.css';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App animate__animated animate__fadeIn">
       <div className="container">
         <TextComponent />
         <div className="row">
-        {pageData.map(item => (
-          <CardComponent cardTitle={item.name} cardParagraph={item.text} cardImg={item.image} className={item.className} />
-        ))}
+          {pageData.map(item => (
+            <CardComponent cardTitle={item.name} cardParagraph={item.text} cardImg={item.image} className={item.className} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/text-component.js
+++ src/components/text-component.js
@@ -1,6 +1,8 @@
+import 'animate.css';
+
 export default function TextComponent(){
     return (
-        <div className="text">
+        <div className="text animate__animated animate__backInDown">
             <h2>Reliable, efficient delivery <span>Powered by Technology</span></h2>
             <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
         </div>
